da3b627e8bbe7ce05a46a2dbd211560045eee770
Ирина Седова <takkakau@mail.ru>
2026-09-07 14:49:28 +0400

    Выполнено задание FitLife

--- fit_life.py
+++ fit_life.py
@@ -1,37 +1,74 @@
 # Проект FitLife - MVP версия 1.0
 print("Привет! Я FitLife-бот, твой помощник в заботе о здоровье!")
 print("Давай познакомимся.\n")
+
+# Константы
+WATER_PER_KG = 30
+ML_PER_LITER = 1000
+SEPARATOR = "=" * 40
+
 # 1. Знакомство
-# TODO: Спроси у пользователя имя и сохрани в переменную user_name
-# TODO: Спроси возраст и сохрани в переменную user_age
-user_name = input("Как тебя зовут? ")
-user_age = int(input("Сколько тебе лет? "))
+while True:
+    user_name = input("Как тебя зовут? ")
+    if user_name.strip():
+        break
+    print("Имя не может быть пустым. Попробуй ещё раз.")
+
+while True:
+    try:
+        user_age = int(input("Сколько тебе лет? "))
+        break
+    except ValueError:
+        print("Пожалуйста, введи число.")
+
 # 2. Сбор данных
-# TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-# TODO: Запроси рост (в метрах, например 1.75) и сохрани в user_height
-user_weight = float(input("Введи свой вес (в кг, например 75.5): "))
-user_height = float(input("Введи свой рост (в метрах, например 1.75): "))
-# 3. Логика расчетов (функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (индекс массы тела)
+while True:
+    try:
+        user_weight = float(input("Введи свой вес (в кг, например 75.5): "))
+        if user_weight > 0:
+            break
+        print("Вес должен быть положительным числом.")
+    except ValueError:
+        print("Пожалуйста, введи число.")
+
+while True:
+    try:
+        user_height = float(
+            input("Введи свой рост (в метрах, например 1.75): ")
+        )
+        if user_height > 0:
+            break
+        print("Рост должен быть положительным числом.")
+    except ValueError:
+        print("Пожалуйста, введи число.")
+
+# 3. Логика расчетов
 bmi = user_weight / (user_height ** 2)
 bmi_rounded = round(bmi, 1)
-# Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
-water_ml = user_weight * 30
-water_needed = round(water_ml / 1000, 1)
+
+# Выбор категории ИМТ
+if bmi_rounded < 18.5:
+    bmi_category = "недостаточная масса тела"
+elif bmi_rounded < 25:
+    bmi_category = "норма"
+elif bmi_rounded < 30:
+    bmi_category = "избыточная масса тела"
+else:
+    bmi_category = "ожирение"
+
+water_ml = user_weight * WATER_PER_KG
+water_needed = round(water_ml / ML_PER_LITER, 1)
+
 # 4. Вывод красивого результата
-# TODO: Выведи приветствие, чтобы вывести приветствие
-# TODO: Выведи возраст, ИМТ (округлённый до 1 знака) и норму воды.
-print("=" * 40)
-print("   ТВОЙ ПЕРСОНАЛЬНЫЙ ОТЧЁТ")
-print("=" * 40)
+print(SEPARATOR)
+print("           ПЕРСОНАЛЬНЫЙ ОТЧЁТ")
+print(SEPARATOR)
 print(f"Имя: {user_name}")
 print(f"Возраст: {user_age} лет")
 print(f"Вес: {user_weight} кг")
 print(f"Рост: {user_height} м")
-print("=" * 40)
-print(f"Индекс массы тела (ИМТ): {bmi_rounded}")
+print(SEPARATOR)
+print(f"Индекс массы тела (ИМТ): {bmi_rounded} ({bmi_category})")
 print(f"Рекомендуемая норма воды: {water_needed} л в день")
-print("=" * 40)
+print(SEPARATOR)
 print("Расчёт окончен. Будьте здоровы!")
